9754d2cf21f90a16a543c26e0df1792238caf0d0
SchneiderMan <jschneid94@gmail.com>
2018-07-11 22:36:06 -0700

    Added code for back to top button.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,6 +15,20 @@ function generateButton() {
     }
 }
 
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 $(document).ready(function () {
 
     generateButton();
@@ -93,5 +107,7 @@ $(document).ready(function () {
             $("#buttonName").val("");
         }
     });
+
+    window.onscroll = function() {scrollFunction()};
 
 });
