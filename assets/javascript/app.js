b2f61c1180f28fb867696f18422bd0bdb5eeba17
SchneiderMan <jschneid94@gmail.com>
2018-07-11 22:40:35 -0700

    Button did not work, will try again.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,20 +15,6 @@ function generateButton() {
     }
 }
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
 $(document).ready(function () {
 
     generateButton();
@@ -107,7 +93,5 @@ $(document).ready(function () {
             $("#buttonName").val("");
         }
     });
-
-    window.onscroll = function() {scrollFunction()};
 
 });
