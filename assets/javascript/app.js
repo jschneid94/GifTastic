9c0858c6616f0af5d59efbea6a1d75d3e7a9519d
SchneiderMan <jschneid94@gmail.com>
2018-07-12 23:01:34 -0700

    Added back to top button.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -94,4 +94,27 @@ $(document).ready(function () {
         }
     });
 
+    // Back to top button appears when scrolling down the page
+    if ($('#back-to-top').length) {
+        var scrollTrigger = 200,
+            backToTop = function () {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > scrollTrigger) {
+                    $('#back-to-top').addClass('show');
+                } else {
+                    $('#back-to-top').removeClass('show');
+                }
+            };
+        backToTop();
+        $(window).on('scroll', function () {
+            backToTop();
+        });
+        $('#back-to-top').on('click', function (e) {
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: 0
+            }, 700);
+        });
+    }
+
 });
